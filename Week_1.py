08fad7bee073f943c611b483796f0d0ee975b2f4
Mabumbe <mabumbepetro@gmail.com>
2025-08-04 12:24:25 +0300

    Week_1 Assignment

---
+++ Week_1.py
@@ -0,0 +1,7 @@
+#Week 1 Assignment
+num1 = float(input("Input the first number: "))
+num2 = float(input("Input the second number: "))
+sum = num1 + num2
+diff = num1 - num2
+print("The summation is: ", sum )
+print("The difference is: ", diff)
