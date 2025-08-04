9cc1f49ba562271900cd6e510fcc5a2faa4ed6d1
Mabumbe <mabumbepetro@gmail.com>
2025-08-04 12:32:23 +0300

    Added Product and Quotient

--- Week_1.py
+++ Week_1.py
@@ -2,6 +2,11 @@
 num1 = float(input("Input the first number: "))
 num2 = float(input("Input the second number: "))
 sum = num1 + num2
-diff = num1 - num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+print("The results are: ")
 print("The summation is: ", sum )
-print("The difference is: ", diff)
+print("The difference is: ", difference)
+print("The product is: ", product)
+print("The quotient is: ", quotient)
